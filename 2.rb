36c03e5022c72b6e12d32262b8a78f4ccb4cc7c5
github@yeahandstuff.net <github@yeahandstuff.net>
2014-03-03 15:04:04 -0500

    Made a little prettier. Ruby shortcuts!

--- 2.rb
+++ 2.rb
@@ -1,12 +1,8 @@
-answer = 0
-
 def fib(num, last, sum)
   return sum if num >= 4_000_000
   sum = sum + num unless num%2 != 0
   fib(num+last, num, sum) 
 end
 
-answer = fib(1,1,0)
-
-puts answer
+puts fib(1,1,0)
 
